13ac066cd1123bd5dade8cb84bdec8ce3cd5333d
senthiltr <36333193+senthiltr@users.noreply.github.com>
2022-01-26 19:26:32 +0530

    adding cypress directory files

---
+++ test_1.spec.js
@@ -0,0 +1,6 @@
+///<referrence type=Cypress />
+describe('test-suite-1', ()=>{
+    it('test1', ()=>{
+        cy.visit('https://167.254.181.71:8443/virtuoranc/index.html');
+    });
+});
---
+++ test_2.spec.js
@@ -0,0 +1,6 @@
+///<referrence type=Cypress />
+describe('test-suite-2', ()=>{
+    it('test2', ()=>{
+        cy.visit('https://167.254.183.23:8443/virtuoranc/index.html');
+    });
+});
